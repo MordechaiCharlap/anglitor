41bd23abda02dc34f8b0665678816d7f9a8507ca
Moti Charlap <74620228+MordechaiCharlap@users.noreply.github.com>
2025-07-17 17:43:43 +0300

    user context+lessons logic

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,21 +1,46 @@
 import Button from "@/components/Button";
-import { ScrollView, Text } from "react-native";
+import { useUser } from "@/context/UserContext";
+import { router } from "expo-router";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Home() {
   const lessons = Array.from({ length: 20 }, (_, i) => `Lesson ${i + 1}`);
-
-  return (
-    <ScrollView contentContainerStyle={{ alignItems: "center" }}>
-      {lessons.map((title, index) => (
-        <Button
-          circle
-          style={{ backgroundColor: "gray", margin: 10 }}
-          key={index}
-          onPress={() => alert(`${title} pressed!`)}
-        >
-          <Text>{title}</Text>
-        </Button>
-      ))}
-    </ScrollView>
-  );
+  const { user, setUser } = useUser();
+  // useEffect(() => {
+  //   setUser();
+  // }, []);
+  if (user) {
+    return (
+      <ScrollView contentContainerStyle={{ alignItems: "center" }}>
+        {lessons.map((title, index) => (
+          <Button
+            circle
+            style={{
+              backgroundColor: user.level < index - 1 ? "gray" : "blue",
+              margin: 10,
+            }}
+            key={index}
+            onPress={() => {
+              if (user.level >= index - 1) router.push("/lesson");
+            }}
+          >
+            <Text
+              style={{
+                color: user.level >= index - 1 ? "white" : "blue",
+                margin: 10,
+                fontWeight: 500,
+              }}
+            >
+              {title}
+            </Text>
+          </Button>
+        ))}
+      </ScrollView>
+    );
+  } else
+    return (
+      <View>
+        <Text>אתה צריך להתחבר בשביל ללמוד</Text>
+      </View>
+    );
 }
