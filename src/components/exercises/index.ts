8c5872a0290f002c6a711c677a66a63eba20f800
Moti Charlap <moredechai.cha@gmail.com>
2025-07-30 12:15:30 +0300

    import page for exercises

---
+++ src/components/exercises/index.ts
@@ -0,0 +1,6 @@
+export { WordBank } from './WordBank';
+export { TranslateEnToHe } from './TranslateEnToHe';
+export { TranslateHeToEn } from './TranslateHeToEn';
+export { ListenEnToTextEn } from './ListenEnToTextEn';
+export { ListenEnToTextHe } from './ListenEnToTextHe';
+export { ExerciseRenderer } from './ExerciseRenderer';
